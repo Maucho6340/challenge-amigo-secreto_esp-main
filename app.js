fc57d6ea7711518c97e12b8ce0db3522c898fac3
Maucho6340 <mauricio.flores@bailacthor.com>
2025-02-15 10:53:52 -0300

    crea el array

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte u
 
 const inputField = document.querySelector('#amigo'); // Selecciona input con id "amigo" y lo almacena en inputField
 const addButton = document.querySelector('.button-add'); // Selecciona botón clase "button-add" y lo almacena en AddButton
+const participantes = []; //crea array para almacenar nombres
 
 function agregarAmigo() {
     const inputValue = inputField.value.trim(); // Captura nombre agregado y elimina espacios en blanco
+    console.log('Nombre ingresado',inputValue);
     if (inputValue === '') {  // Valida que inputValue no esté vacío
       alert('Por favor, inserte un nombre.'); // Si está vacío muestra mensaje
     } else {
+      participantes.push (inputValue); //añade nombre validado al array amigos
+      console.log ('Participantes', participantes); // muestra participantes en consola
         inputField.value = ''; // Deja limpio el campo de entrada una vez capturado el valor por id "amigo"
     }
 }
-// Asigna la función al evento click del botón
-addButton.addEventListener('click', agregarAmigo);
+addButton.addEventListener('click', agregarAmigo); // Asigna la función al evento click del botón
+
+/*Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena 
+los nombre de amigos usando el método.push().*/
