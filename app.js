2823a9634948602be5928e7743fb62ee2017864e
Maucho6340 <mauricio.flores@bailacthor.com>
2025-02-15 10:30:21 -0300

    Captura texto y botón y mofica html

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ o document.querySelector para obtener el texto ingresado por el usuario.*/
 /*2do: Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. 
 Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."*/
 
-const inputField = document.querySelector ('#amigo'); //captura el valor del campo con id amigo
-const AddButton = documento.querySelector ('.button-add') //captura el botón con clase button-add
+const inputField = document.querySelector('#amigo'); // Selecciona input con id "amigo" y lo almacena en inputField
+const addButton = document.querySelector('.button-add'); // Selecciona botón clase "button-add" y lo almacena en AddButton
 
-function agregarAmigo() { //declara función para ingresar nombre
-   const inputValue = inputField.value.trim(); //captura valor ingresado y lo guarda en inputValue sin espacios en blanco
-   if (inputValue == ''){ //verifica que inputValue no esté vacío
-      alert ('Por favor, inserte un nombre.'); //si está vacío, emite mensaje
-   } else {
-      console.log ('valor ingresado:', inputValue); // o si no: mensaje en consola con el valor agregado
-      inputField = ''; //deja limpio el campo para un nuevo ingreso
-   }
+function agregarAmigo() {
+    const inputValue = inputField.value.trim(); // Captura nombre agregado y elimina espacios en blanco
+    if (inputValue === '') {  // Valida que inputValue no esté vacío
+      alert('Por favor, inserte un nombre.'); // Si está vacío muestra mensaje
+    } else {
+        inputField.value = ''; // Deja limpio el campo de entrada una vez capturado el valor por id "amigo"
+    }
 }
+// Asigna la función al evento click del botón
+addButton.addEventListener('click', agregarAmigo);
