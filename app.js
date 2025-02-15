f068e7d8b563294aa3cde60b583df52c8265e05b
Maucho6340 <mauricio.flores@bailacthor.com>
2025-02-15 17:19:54 -0300

    Actualiza array y borra imagen web

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte u
 
 const inputField = document.querySelector('#amigo'); // Selecciona input con id "amigo" y lo almacena en inputField
 const addButton = document.querySelector('.button-add'); // Selecciona botón clase "button-add" y lo almacena en AddButton
+const listaAmigos = document.queryelector ('#listaAmigos'); //selecciona el contenedor <ul> con id listaAmigos
 const participantes = []; //crea array para almacenar nombres
+
 
 function agregarAmigo() {
     const inputValue = inputField.value.trim(); // Captura nombre agregado y elimina espacios en blanco
@@ -17,9 +19,18 @@ function agregarAmigo() {
     } else {
       participantes.push (inputValue); //añade nombre validado al array amigos
       console.log ('Participantes', participantes); // muestra participantes en consola
-        inputField.value = ''; // Deja limpio el campo de entrada una vez capturado el valor por id "amigo"
+      inputField.value = ''; // Deja limpio el campo de entrada una vez capturado el valor por id "amigo"
     }
 }
+function actualizarListaDeAmigos (){
+  listaAmigos.innerHTML=''; // Asigna cadena vacía al contenido HTML del contenedor <ul> con id "listaAmigos"
+participantes.forEach((listaAmigos,index)=>{ //Recorre cada elemento del array 
+  const amigoElement = document.createElement ('li'); // Añade uno nuevo elemento "li" a la lista
+  amigoElement.textContent = `${index + 1}. ${amigo}`; // Crea la cadena de nombres
+  listaAmigos.appendChild(amigoElement); //añade nuevo elemento al contenedor
+  });
+  }
+
 addButton.addEventListener('click', agregarAmigo); // Asigna la función al evento click del botón
 
 /*Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena 
